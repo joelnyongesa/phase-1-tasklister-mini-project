b4f5aee96cbaaf1ddec69932f05ac38c809cc791
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-21 23:51:46 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addtask();
+    form.reset()
+  })
 });
+
+// Add a task
+function addtask(todo){
+  let newTask = document.querySelector("#new-task-description").value
+  let li = document.createElement('li')
+  li.append(newTask);
+  let btn = document.createElement('button');
+  btn.textContent = ' x '
+  btn.addEventListener('click', handleDelete)
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+
+// Handling delete
+function handleDelete(e){
+  console.log(e.target.parentNode.remove())
+}
